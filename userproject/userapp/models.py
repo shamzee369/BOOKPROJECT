bac55bb84c1486f284d004c8f19007056b7d5685
shamzee369 <shamseershavpm939@gmail.com>
2024-09-23 11:37:32 +0530

    Delete userproject directory

--- userproject/userapp/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-class User(models.Model):
-
-    name=models.CharField(max_length=300,null=True)
-    email=models.CharField(max_length=300,null=True)
-
-    def __str__(self):
-        return '{}',format(self.name)
